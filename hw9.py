e50fdd1864d9380e3c66ebc484c3ad0f7601a737
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-05-25 18:18:52 +0300

    hw done

--- hw9.py
+++ hw9.py
@@ -10,26 +10,82 @@
 Наприклад, для суми 113 це буде словник {1: 1, 2: 1, 10: 1, 50: 2}
 
 """
+from timeit import timeit
+
+coins = [50, 25, 10, 5, 2, 1]
 
 def find_coins_greedy(input_sum):
-    """Find coints by greedy alg"""
-    coints = {}
-    iterator = iter(monets)
+    """Find coins by greedy alg"""
+    result = {}
+    iterator = iter(coins)
     current = next(iterator)
     while input_sum > 0:
         if current <= input_sum:
             input_sum = input_sum - current
-            coints[current] = coints[current] + 1 if current in coints else 1
+            result[current] = result[current] + 1 if current in result else 1
         else:
             current = next(iterator)
-    return coints
+    return result
 
 def find_min_coins(input_sum):
-    pass
+    """Find coins using dynamic programming"""
+    # Ініціалізація масиву для зберігання мінімальної кількості монет для кожної суми
+    min_coins = [float('inf')] * (input_sum + 1)
+    # базовий випадок
+    min_coins[0] = 0
+    # пустий список для зберігання останньої використоаної монети
+    last_used = [0] * (input_sum + 1)
+    result = {}
+    for x in range(1, input_sum + 1):
+        # print(x, "Min coins:\n", min_coins, "\nLast used\n", last_used)
+        if x in coins:
+            min_coins[x] = 1
+            last_used[x] = x
+            continue
+        last = x - 1
+        min_coins[x] = min_coins[last] + 1
+        last_used[x] = last_used[last]
+
+    result = {}
+    while input_sum > 0:
+        coin = last_used[input_sum]
+        result[coin] = result[coin] + 1 if coin in result else 1
+        input_sum -= coin
+    return result
 
 
-monets = [50, 25, 10, 5, 2, 1]
+def test_greedy(input_sum):
+    """Greedy alg test function"""
+    find_coins_greedy(input_sum)
 
-example_sum = 113 # input("Input sum to calculate")
-print(f"Input sum: {example_sum}, monets:{find_coins_greedy(example_sum)}")
-print(f"Input sum: {example_sum}, monets:{find_min_coins(example_sum)}")
+def test_dynamic(input_sum):
+    """Dynamic alg test function"""
+    find_coins_greedy(input_sum)
+
+def run_test(func_part_name, input_sum, times=100):
+    """Run test function"""
+    print(f"Testing algoritm {alg_names[func_part_name]}" +\
+          f" for input sum {input_sum} ......")
+    print("" * 100)
+    elapsed_time = timeit(
+        f"test_{func_part_name}(input_sum)",
+        setup=f"from __main__ import test_{func_part_name}",
+        number=times, globals=globals()
+    )
+    return f"Result for algoritm {alg_names[func_part_name]}" +\
+           f" elapsed time:{elapsed_time}"
+
+alg_names = {
+    "greedy" : "Жадібний алгортим",
+    "dynamic" : "Динамічне програмування",
+}
+
+for func_short_name in alg_names:
+    for input_sum in [113, 2757, 93232, 96273232]:
+        print(run_test(func_short_name, input_sum, 100))
+        print("=" * 100)
+
+for input_sum in [113, 2757, 93232, 96273232]:
+    print("*" * 100)
+    print(f"Result for {input_sum} by greedy", find_coins_greedy(input_sum))
+    print(f"Result for {input_sum} by dynamic", find_min_coins(input_sum))
